7bee554f3f2bef10d280d01ae43b828405e7aef7
Venkatesh Sambandamoorthy <ven0226@gmail.com>
2015-11-26 20:38:31 -0500

    Update installTools.bat

--- build-vm/installTools.bat
+++ build-vm/installTools.bat
@@ -1,3 +1,19 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/SoftwareEngineeringToolDemos/ICSE-2013-MCT/raw/master/test/eclipse.zip', 'C:\Users\IEUser\Desktop\eclipsegitty.zip')"
+@powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.dropbox.com/s/bmxf8jq7gj4gber/eclipse.zip?dl=1', 'C:\Users\IEUser\Desktop\eclipse.zip')"
 
-powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('C:\Users\IEUser\Desktop\eclipsegitty.zip', 'C:\Users\IEUser\Desktop\eclipse'); }"
+@powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=0B5-WRa86uKb_bXBCNFFWTDRHTHc', 'C:\Users\IEUser\Desktop\Readme.txt')"
+@powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=0B5-WRa86uKb_Qm1oUElwWmNyTUE', 'C:\Users\IEUser\Desktop\License.txt')"
+@powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=0B5-WRa86uKb_Qlc0Z3l1RHNUV28', 'C:\Users\IEUser\Desktop\Installation.txt')"
+
+
+set PATH=%PATH%;C:\Program Files\7-Zip\
+
+7z x C:\Users\IEUser\Desktop\eclipse.zip -oC:\Users\IEUser\Desktop\eclipse
+
+@echo off
+
+(echo [InternetShortcut]
+echo URL=https://www.youtube.com/watch?v=RnW_6QKTBko
+echo IconIndex=0) >"%userprofile%\desktop\MCT Demo Video.url"
+
+mkdir c:\workspace
+C:\Users\IEUser\Desktop\eclipse\eclipse\eclipse.exe -clean -data "c:\workspace"
