6cb913cf7ebca7e0e79a1eb5235488d35f4177f3
Venkatesh Sambandamoorthy <ven0226@gmail.com>
2015-11-28 13:47:51 -0500

    updated powerscript

---
+++ build-vm/vagrant_setup.ps1
@@ -0,0 +1,20 @@
+# Powershell Script to prepare the windows install to be used with vagrant-windows
+
+# https://gist.githubusercontent.com/tvjames/6750255/raw/7e5b4ada30ce7366486e04933035d50d341b02b8/vagrant_prepare.ps1
+
+Set-ExecutionPolicy -executionpolicy remotesigned -force
+
+# Step 1: Disable UAC
+New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA -PropertyType DWord -Value 0 -Force | Out-Null
+Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green
+
+# Step 4: Disable Automatic Updates
+# Reference: http://www.benmorris.me/2012/05/1st-test-blog-post.html
+$AutoUpdate = (New-Object -com "Microsoft.Update.AutoUpdate").Settings
+$AutoUpdate.NotificationLevel = 1
+$AutoUpdate.Save()
+Write-Host "Windows Update has been disabled." -ForegroundColor Green
+
+# Step 8: Disable Windows Firewall
+&netsh "advfirewall" "set" "allprofiles" "state" "off"
+Write-Host "Windows Firewall has been disabled." -ForegroundColor Green
